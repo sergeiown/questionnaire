482bf88e69c24edf5f443c5cee41bee9803b4bfa
Serhii Myshko <sergeiown@gmail.com>
2023-03-14 02:01:37 +0200

    improve color picker

--- src/components/AdminArea/PostlistColor.jsx
+++ src/components/AdminArea/PostlistColor.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MyColorPicker from '../UI/colorPicker/MyColorPicker';
 
 const PostListColor = ({ color, setColor }) => {
+    const handleColorChange = (e) => {
+        setColor({ ...color, query: e.target.value });
+        e.target.blur(); /* remove focus from input */
+    };
+
     return (
         <div>
-            <MyColorPicker
-                value={color.query || color.new}
-                onInput={(e) => setColor({ ...color, query: e.target.value })}
-            />
+            <MyColorPicker value={color.query || color.new} onChange={handleColorChange} />
         </div>
     );
 };
