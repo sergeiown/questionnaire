029c4b85a5bc8382080b01d53cb12568150b9ee5
Serhii Myshko <sergeiown@gmail.com>
2023-04-13 15:17:14 +0300

    add preloader to the user area

--- src/components/UserArea/SurveyQuestions.jsx
+++ src/components/UserArea/SurveyQuestions.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import PostService from '../../API/PostService';
 import SurveyAnswers from './SurveyAnswers';
+import Loader from '../UI/Loader/Loader';
 
 const SurveyQuestions = ({ color, questionsSubmit }) => {
     const [questions, setQuestions] = useState([]);
     const date = new Date().toLocaleDateString('uk-UA');
     const time = new Date().toLocaleTimeString('uk-UA', { hour: '2-digit', minute: '2-digit' });
     const [answers, setAnswers] = useState([date, time]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
     const fetchSavedQuestions = async () => {
         const questions = await PostService.get();
         setQuestions(questions);
+        if (isLoading) {
+            setTimeout(() => setIsLoading(false), 1000);
+        }
     };
 
     const handleNextQuestion = () => {
@@ -35,7 +40,11 @@ const SurveyQuestions = ({ color, questionsSubmit }) => {
         }
     }, [answers]);
 
-    return (
+    return isLoading ? (
+        <div className="loader">
+            <Loader />
+        </div>
+    ) : (
         <div className="questionsWrapper">
             {questions.length > 0 ? (
                 <>
